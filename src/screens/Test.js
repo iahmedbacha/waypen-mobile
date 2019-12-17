394c92b9e42e50766bf0fed77584152dd499d5c5
Ibrahim AHMED BACHA <fi_ahmed_bacha@esi.dz>
2019-12-17 05:02:12 +0100

    load data in test screen

--- src/screens/Test.js
+++ src/screens/Test.js
@@ -1,15 +1,28 @@
 import React, { Component } from 'react';
-import { Button, Text, StyleSheet, View } from 'react-native';
+import { Text, StyleSheet, View } from 'react-native';
 
 class Test extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: null
+      data: null,
+      isLoaded: false
     };
   }
 
+  componentDidMount() {
+    const { navigation } = this.props;
+    this.setState({data: navigation.getParam('data')});
+
+    if (!this.data) {
+      this.setState({ isLoaded: true });
+    }
+  }
+
   render() {
+    if (!this.state.isLoaded) {
+      return null;
+    }
     return (
       <View styles={styles.test}>
         <Text>Test Page</Text>
